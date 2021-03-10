e88e5dcc2080f64bf9b99127a94c96e5c0545e72
Cristiano <cristiano.tarconi@gmail.com>
2021-03-10 20:00:08 +0100

    products page structure w/ style & animations

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title', 'Molisana Laravel Test')</title>
+    {{-- Link Font Awesome --}}
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
     {{-- Link Google Font --}}
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Teko:wght@300;600&display=swap" rel="stylesheet">
--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -3,5 +3,15 @@
 @section('title', 'Prodotti | La Molisana')
 
 @section('content')
-    <h1>PRODOTTI</h1>
+    <div class="products_container">
+        @foreach ($pasta as $item)
+            <div class="box_pasta">
+                <img src="{{$item['src']}}" alt="{{$item['titolo']}}">
+                <div class="overlay">
+                    <i class="fas fa-plus"></i>
+                    <p>{{$item['titolo']}}</p>
+                </div>
+            </div>
+        @endforeach
+    </div>
 @endsection
